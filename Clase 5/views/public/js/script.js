609d3aaab396867d761d5afa851a884f55f57057
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-10-31 16:15:58 -0500

    nueva clase

---
+++ Clase 5/views/public/js/script.js
@@ -0,0 +1,9 @@
+function Persona(nombre, edad, estatura, color){
+    this.nombre
+    this.edad
+    this.estatura
+    this.color
+}
+                       
+let perosnaUno = new Persona('douglas',20,1.70,verde
+console.log persona uno?de 
